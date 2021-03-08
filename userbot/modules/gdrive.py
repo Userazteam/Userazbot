c547e73fbf4652d46247fcb7ca3ff58ef92f5f38
Userazteam <78642036+Userazteam@users.noreply.github.com>
2021-03-08 17:57:24 +0400

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -259,7 +259,7 @@ async def upload_file(http, file_path, file_name, mime_type, event, parent_id):
     media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
     body = {
         "title": file_name,
-        "description": "Brend UserBot istifadə edərək quraşdırılmışdır.",
+        "description": "@Useraz istifadə edərək quraşdırılmışdır.",
         "mimeType": mime_type,
     }
     if parent_id:
